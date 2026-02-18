18a829aa186ad102e2dc44c4790a79a00481c5ec
VasFruzsina <vs.frzsn@gmail.com>
2026-02-18 18:47:07 +0100

    base

--- demo_projects/app/page.tsx
+++ demo_projects/app/page.tsx
@@ -1,7 +1,17 @@
+import DragDrop from "@/components/ui/drag&drop";
+
 export default function Page() {
   return (
-    <div>
-      Hello World
-    </div>
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+      </head>
+      <body>
+        {/* <header className="mt-150">Wannabe menu</header> */}
+        <DragDrop />
+      </body>
+    </html>
   );
 }
